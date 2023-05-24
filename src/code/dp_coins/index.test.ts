4bcfd3afdea755f2c5c31f5979d84cabda7f30ef
frankqiwu <frankqiwu@tencent.com>
2023-05-24 11:30:55 +0800

    feat: dp_coins筹零钱问题的

--- src/code/dp_coins/index.test.ts
+++ src/code/dp_coins/index.test.ts
@@ -1,6 +1,9 @@
-import { coinChange } from './index'
+import coinChange from './index'
 
-test('fib 测试 N = 1', () => {
-    coinChange([1, 2, 5, 10], 11);
-    expect(1).toEqual(1)
+test('coinChange 凑硬币计算', () => {
+    const res1 = coinChange([1, 2, 5, 10], 11);
+    expect(res1).toEqual(2)
+
+    const res2 = coinChange([1, 2, 5], 11);
+    expect(res2).toEqual(3)
 });
--- src/code/dp_coins/index.ts
+++ src/code/dp_coins/index.ts
@@ -1,12 +1,15 @@
-export function coinChange(coins: number[], amount: number): number {
-    // base case
-    if (amount < 0) return -1
-    if(amount === 0) return 0
-    // 状态转移方程
-    let res = Infinity;
-    for(let coin of coins) {
-        console.log({ coin })
+// dp 数组的定义：当目标金额为 i 时，至少需要 dp[i] 枚硬币凑出
+const coinChange = (coins: number[], amount: number): number => {
+    const dpArray = new Array(amount + 1).fill(Infinity);
+    dpArray[0] = 0;  //base case
+    for(let i=0; i<dpArray.length; i++) {
+        // 状态转移方程
+        for(let j=0; j<coins.length; j++) {
+            if(i < coins[j]) continue;
+            dpArray[i] = Math.min(dpArray[i], 1 + dpArray[i - coins[j]])
+        }
     }
-
-    return 1
+    return (dpArray[amount] == Infinity) ? -1 : dpArray[amount]
 };
+
+export default coinChange;
